b5eaecf8cf51858badaa1543f85cb94ddf40f0e2
Honey Jhamnani <honeyjhamnani410@gmail.com>
2026-03-24 17:55:50 +0530

    RED: failing test for salary metrics

---
+++ spec/requests/api/v1/salary_metrics_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Salary Metrics", type: :request do
+  it "returns country metrics" do
+    create(:employee, country: "India", salary: 1000)
+    create(:employee, country: "India", salary: 2000)
+
+    get "/salary_metrics/country", params: { country: "India" }
+
+    json = JSON.parse(response.body)
+
+    expect(json["min"]).to eq(1000.0)
+    expect(json["max"]).to eq(2000.0)
+    expect(json["average"]).to eq(1500.0)
+  end
+end
