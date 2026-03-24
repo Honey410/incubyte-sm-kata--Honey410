82e9df962b0400837827a68f26e8bbe23bfad9a0
Honey Jhamnani <honeyjhamnani410@gmail.com>
2026-03-24 18:13:37 +0530

    REFACTOR: add min and max salary

---
+++ app/controllers/api/v1/salary_metrics_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::SalaryMetricsController < ApplicationController
+
+  def by_country
+    employees = Employee.where(country: params[:country])
+
+    render json: {
+      min: employees.minimum(:salary)&.to_f,
+      max: employees.maximum(:salary)&.to_f,
+      average: employees.average(:salary)&.to_f
+    }
+  end
+
+  def by_job_title
+    employees = Employee.where(job_title: params[:job_title])
+
+    render json: {
+      average: employees.average(:salary)&.to_f
+    }
+  end
+end
